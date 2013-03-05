9e900d3e192b8fe792c77593df71ef98616905d7
Ian Cooper <cooperia.ian@gmail.com>
2013-03-04 18:11:28 -0800

    Fixed test for click Cell view click event.

---
+++ spec/javascripts/cellView_spec.js
@@ -0,0 +1,27 @@
+describe("TicTac.Views.Cell", function(){
+
+  describe('event: click .cell', function(){
+    it ("when clicked would call placeX", function(){
+      var placeXSpy = spyOn(TicTac.Views.Cell.prototype, 'placeX')
+      var currentCell = new TicTac.Views.Cell({model: new TicTac.Models.Cell})
+      currentCell.render()
+      currentCell.$el.click();
+      expect(placeXSpy).toHaveBeenCalled();
+    })
+  })
+
+  describe('#placeX', function(){
+    var currentView, currentModel
+    beforeEach(function(){
+      currentModel = new TicTac.Models.Cell
+      currentView = new TicTac.Views.Cell({model: currentModel})
+    })
+    it("it places an X in the clicked div and updates the associated model", function(){
+      var modelValueSpy = spyOn(currentModel, 'setX');
+      currentView.placeX();
+      expect(modelValueSpy).toHaveBeenCalled()
+    })
+  })
+
+
+});
